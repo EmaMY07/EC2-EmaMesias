92d5618be5dfd0b1a18ccadbcc2c6d4dc70f3486
Ema Mesias Yacza <117551059+EmaMY07@users.noreply.github.com>
2023-07-10 14:01:30 +0000

    RepositoryRestResource

--- src/main/java/com/examen/ec2/CursoRepository.java
+++ src/main/java/com/examen/ec2/CursoRepository.java
@@ -1,5 +1,11 @@
 package com.examen.ec2;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
-public interface CursoRepository extends CrudRepository<Curso,Integer>{
-    
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource(collectionResourceRel = "cursos",path = "cursos")
+public interface CursoRepository extends CrudRepository<Curso,Integer>,PagingAndSortingRepository<Curso,Integer>{
+    List<Curso>findByNombre(@Param("nombre") String nombre);
 }
